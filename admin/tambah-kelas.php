a4c10998574d2b9bebfa06f774bb3b04a3d6d8aa
KeiClaire <herysuparjan550@gmail.com>
2024-05-12 13:51:32 +0800

    menambahkan fungsi kelas

---
+++ admin/tambah-kelas.php
@@ -0,0 +1,17 @@
+<h5>Halaman Tambah Data Kelas</h5>
+<a href="?url=kelas" class="btn btn-primary">Kembali</a>
+<hr>
+<form method="post" action="?url=proses-tambah-kelas">
+    <div class="form-groub mb-2">
+        <label>Nama Kelas</label>
+        <input type="text" name="nama_kelas" class="form-control" required>
+    </div>
+    <div class="form-groub mb-2">
+        <label>Kompetensi Keahlian</label>
+        <input type="text" name="kompetensi_keahlian" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <button type="sumbit" class="btn btn-success">Simpan</button>
+        <button type="reset" class="btn btn-warning">Kosongkan</button>
+    </div>
+</form>
